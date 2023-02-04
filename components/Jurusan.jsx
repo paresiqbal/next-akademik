023ac91807824b27e6e4fe63f672d95cda4161b3
paresiqbal <thecrowdel@gmail.com>
2023-02-04 18:11:50 +0700

    "Bug Carousel please fix"

--- components/Jurusan.jsx
+++ components/Jurusan.jsx
@@ -5,29 +5,97 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/swiper.min.css";
+import "swiper/css";
+import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 // import required modules
 import { FreeMode, Pagination } from "swiper";
-import Link from "next/link";
 
-export default function App() {
+export default function Jurusan() {
   return (
-    <div>
+    <div className="container py-4 ">
       <Swiper
-        slidesPerView={3}
+        breakpoints={{
+          // when window width is >= 640px
+          320: {
+            width: 320,
+            slidesPerView: 1,
+          },
+          // when window width is >= 768px
+          768: {
+            width: 768,
+            slidesPerView: 3,
+          },
+        }}
+        slidesPerView={1}
         spaceBetween={30}
         freeMode={true}
         pagination={{
           clickable: true,
         }}
         modules={[FreeMode, Pagination]}
-        className="mySwiper container"
+        className="mySwiper"
       >
         <SwiperSlide>
-          <img src="/image/Cardhome/jurusan1.jpg" className="w-full" />
-          <Link href={"/"}>Teknik Komputer Jaringan</Link>
+          <Image
+            src={"/image/Cardhome/jurusan1.jpg"}
+            className="rounded-xl"
+            alt="aaa"
+            width={2000}
+            height={20}
+          />
+          <h1 class="absolute text-center text-3xl font-semibold text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Teknik Komputer Jaringan
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src={"/image/Cardhome/jurusan2.jpg"}
+            className="rounded-xl"
+            alt="aaa"
+            width={2000}
+            height={20}
+          />
+          <h1 class="absolute text-center text-3xl font-semibold text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Multimedia
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src={"/image/Cardhome/jurusan3.jpg"}
+            className="rounded-xl"
+            alt="aaa"
+            width={3000}
+            height={40}
+          />
+          <h1 class="absolute text-center text-3xl font-semibold text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Perkantoran
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src={"/image/Cardhome/jurusan2.jpg"}
+            className="rounded-xl"
+            alt="aaa"
+            width={2000}
+            height={20}
+          />
+          <h1 class="absolute text-center text-3xl font-semibold text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Akuntansi
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src={"/image/Cardhome/jurusan5.jpg"}
+            className="rounded-xl"
+            alt="aaa"
+            width={2000}
+            height={20}
+          />
+          <h1 class="absolute text-center text-3xl font-semibold text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Jasaboga
+          </h1>
         </SwiperSlide>
       </Swiper>
     </div>
